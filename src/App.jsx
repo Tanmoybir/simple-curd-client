254b38c8456f0315ef7a96f3818ffb3057f2e849
Tanmoy Bir <tanmoy7468@gmail.com>
2023-12-25 17:25:23 +0600

    apply put method successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { NavLink } from 'react-router-dom';
 import './App.css'
 
 function App() {
@@ -7,36 +8,43 @@ function App() {
     const form = e.target;
     const name = form.name.value;
     const email = form.email.value;
-    const users = {name,email};
+    const users = { name, email };
     console.log(users);
     fetch('http://localhost:5000/users', {
       method: 'POST',
-      headers:{
+      headers: {
         'content-type': 'application/json'
       },
-      body:JSON.stringify(users)
+      body: JSON.stringify(users)
     })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      if(data.insertedId) {
-        alert('User Added Successfully')
-        form.reset()
-      }
-    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.insertedId) {
+          alert('User Added Successfully')
+          form.reset()
+        }
+      })
 
   }
 
   return (
     <>
       <h1>SIMPLE CRUD</h1>
-     <form onSubmit={handleAddUser}>
-      <input type="text" name="name" id="" />
-      <br />
-      <input type="email" name="email" id="" />
-      <br />
-      <input type="submit" value="Add User" />
-     </form>
+
+      <nav>
+        <ul>
+          <li><NavLink to={'/'}>Home</NavLink></li>
+          <li><NavLink to={'/users'}>User</NavLink></li>
+        </ul>
+      </nav>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   )
 }
--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -1,15 +1,41 @@
+import { useLoaderData } from "react-router-dom";
 
 
 const Update = () => {
-    
+    const user = useLoaderData()
+   
+    const handleUpdate = (e) => {
+        e.preventDefault()
+        const form = e.target
+        const name = form.name.value
+        const email = form.email.value
+        const upDateUser ={name,email}
+        console.log(upDateUser);
+        fetch(`http://localhost:5000/users/${user._id}`,{
+            method: 'PUT',
+            headers: {
+                "content-type" : "application/json"
+            },
+            body:JSON.stringify(upDateUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.modifiedCount) {
+                alert('User Updated Successfully')
+                form.reset()
+                
+            }
+        })
+    } 
    
     return (
         <div>
-           <h3>Update information of </h3> 
-           <form>
-      <input type="text" name="name" id="" />
+           <h3>Update information of {user.name}</h3> 
+           <form onSubmit={handleUpdate}>
+      <input type="text" name="name" defaultValue={user?.name} id="" />
       <br />
-      <input type="email" name="email" id="" />
+      <input type="email" name="email" defaultValue={user?.email} id="" />
       <br />
       <input type="submit" value="Update" />
      </form>
